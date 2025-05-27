4cb1891344d9815a64a1616388179fa534c094a4
Palak aggarwal <palakaggarwalaol@gmail.com>
2025-05-27 22:09:15 +0530

    Navbar created

---
+++ portfolio-website/src/Navbar.jsx
@@ -0,0 +1,20 @@
+import './Home/home.css'
+
+function Navbar(){
+    return(
+<>
+   <div class="desktop-1">
+    		<b class="contact">CONTACT</b>
+    		<b class="works">WORKS</b>
+    		<div class="desktop-1-inner">
+    		</div>
+    		<b class="home">HOME</b>
+    		<b class="about">ABOUT</b>
+    		<img class="vector-icon1" alt="" src="Vector.svg"></img>
+    </div>
+</>
+)
+
+}
+
+export default Navbar;
